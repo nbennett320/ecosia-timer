80f9dbcf2fc52b646542c3bf620099e55e2e7338
Noah Bennett <nbennett.320@gmail.com>
2020-04-25 14:09:54 -0400

    fix bug where class properties didnt work, added footer

--- src/content_scripts/Main.js
+++ src/content_scripts/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Footer from './Footer'
 
 export default class Main extends Component {
     render() {
         const { query } = this.props
         return (
             <div className="widget-container" style={styles.main}>
+                {/* put widget content here */}
                 <p className="widget-result"> {query} </p>
+                <Footer />
             </div>
         )
     }
